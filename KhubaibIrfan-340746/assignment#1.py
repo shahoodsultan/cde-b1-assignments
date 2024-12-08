19f3ae7166cbbab2c76da1828339df6e2c8d9ff5
khubaib923 <khubaibirfan199@gmail.com>
2024-12-08 15:12:37 +0500

    committed

---
+++ KhubaibIrfan-340746/assignment#1.py
@@ -0,0 +1,69 @@
+# Print Your Name with your Father name and Date of birth using suitable escape sequence charactor
+
+print("My name is Muhammad Khubaib Irfan and Date of birth is 26-November-1999")
+
+# Write your small bio using variables and print it using print function
+
+bio="My name is khubaib Irfan. I am 25 years old. I have graduated from University Of Karachi"
+print(bio)
+
+# Write a program in which use all the operators we can use in Python
+
+a=10
+b=20
+sum=a+b
+sub=a-b
+mul=a*b
+div=a/b
+rem=a%b
+print(sum,sub,mul,div,rem)
+
+
+# Completes the following steps of small task:
+# Mention Marks of English , Islamiat and Maths out of 100 in 3 different variables
+# Mention Variable of Total Marks and assign 300 to it
+# Calculate Percentage
+
+
+english_marks=70
+islamiat_marks=80
+math_marks=90
+total_marks=300
+obtained_marks=english_marks+islamiat_marks+math_marks
+percentage=(obtained_marks/total_marks)*100
+print(percentage)
+
+# A company decided to give bonus of 5% to employee if his/her year of service is more than 5 years. Ask user for their salary and year of service and print the net bonus amount.
+
+# year_of_service=int(input("Please write your year of service"))
+# if year_of_service>5:
+#     salary=int(input("Please write your salary"))
+#     bonus=salary*(5/100)
+#     print(f"Your bonus amount is {bonus}")
+    
+
+# else:
+#     print("You are not eligible to get bonus")
+
+
+# Write a program to check whether a person is eligible for voting or not. (accept age from user) if age is greater than 17 eligible otherwise not eligible
+
+# age=int(input("Please write your age"))
+# if age>17:
+#     print("You are eligible to vote")
+# else:
+#     print("You are not eligible to vote")
+
+# Write a program to check whether a number entered by user is even or odd.
+
+# number=int(input("Please enter number"))
+# if number%2==0:
+#     print("Even number")
+# else:
+#     print("odd number")
+
+
+
+
+
+
